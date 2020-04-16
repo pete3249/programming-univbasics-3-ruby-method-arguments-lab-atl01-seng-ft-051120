fe6b7b8fc306210c9458274f0ee962432ed1c52d
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-16 15:55:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,17 +7,19 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+introduction("Josh")
+
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
-def introduction_with_language_optional(name, language = "Ruby")
+def introduction_with_language_optional(name, language ="Ruby")
   puts "Hi my name is #{name} and I am learning to prgram in #{language}."
 end
 
-
+introduction_with_language_optional("Edwin")
 
 
 
